6ba5c2de2b210cb88aaf3d8e46c85d3fdab32e15
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-07 12:15:31 +0530

    FakeHEADLIneGenerator

---
+++ fakeheadlinegenerator.py
@@ -0,0 +1,45 @@
+#1- import the random module
+
+import random
+
+#2 - creating subjects
+
+subjects=[
+    "vikash",
+    "Not",
+    "luxxy",
+    "zuxxy",
+    "A group of founders"
+    "A alone leader"
+]
+actions = [
+    "launches",
+    "created",
+    "builded",
+    "changing",
+    "created billion dollar company",
+    "will lead the world and change  the world"
+]
+places_or_thing = [
+    "rocket in the india",
+    "a robot in the bangalore",
+    "rocket",
+    "the world",
+    "at the age of 22",
+    "and make the humanity multiplanetry"
+]
+#3 start the headline generation loop
+while True:
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place_or_thing = random.choice(places_or_thing)
+
+    headline = f" BREAKING NEWS : {subject} {action} {places_or_thing}"
+    print("\n" + headline)
+
+    user_input = input("\nDo you want another headline? (yes/no)").strip().lower()
+    if user_input == "no":
+        break
+
+        #printing the  goodbye message
+print("\n Thanks for using the fake news Headline gEnerator . Have a fun day")
